ce8ca84cc662b140f9099a98fb9cbf8c6a184f60
vikzh <vikt.anmail@gmail.com>
2021-03-20 20:02:31 +0300

    cors and tests added

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3d party
     'rest_framework',
+    'corsheaders',
 
     # Local
     'todos',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,3 +132,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
--- backend/todos/tests.py
+++ backend/todos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title="First Todo", body="Some Text")
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEqual(expected_object_name, "First Todo")
+
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_body = f'{todo.body}'
+        self.assertEqual(expected_object_body, "Some Text")
